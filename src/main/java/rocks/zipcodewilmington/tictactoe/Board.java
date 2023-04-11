33e4f3bcd63c7733b886d4b6d6da954e5bb8f1be
AnastasiaE3 <125825396+AnastasiaE3@users.noreply.github.com>
2023-04-11 19:35:09 -0400

    winner found, tricky return with empty string, but i got it

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,7 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
-    private  Character[][] matrix;
+    private Character[][] matrix;
 
     public Board(Character[][] matrix) {
         this.matrix = matrix;
@@ -12,19 +12,19 @@ public class Board {
 
     public Boolean isInFavorOfX() {
         for (int i = 0; i < 3; i++) {
-            if (matrix[i][0] == 'X' && matrix [i][1] == 'X' && matrix [i][2] == 'X') {
+            if (matrix[i][0] == 'X' && matrix[i][1] == 'X' && matrix[i][2] == 'X') {
                 return true; //checking rows
             }
         }
         for (int i = 0; i < 3; i++) {
-            if (matrix[0][i] == 'X' && matrix [1][i] == 'X' && matrix [2][i] == 'X') {
+            if (matrix[0][i] == 'X' && matrix[1][i] == 'X' && matrix[2][i] == 'X') {
                 return true; //checking columns
             }
         }
-        if (matrix[0][0] == 'X' && matrix [1][1] == 'X' && matrix [2][2] == 'X') {
+        if (matrix[0][0] == 'X' && matrix[1][1] == 'X' && matrix[2][2] == 'X') {
             return true;
         }
-        if (matrix[0][2] == 'X' && matrix [1][1] == 'X' && matrix [2][0] == 'X') {
+        if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X') {
             return true; //checking diagonals
         }
         return false; //no winnings for x
@@ -32,19 +32,19 @@ public class Board {
 
     public Boolean isInFavorOfO() {
         for (int i = 0; i < 3; i++) {
-            if (matrix[i][0] == 'O' && matrix [i][1] == 'O' && matrix [i][2] == 'O') {
+            if (matrix[i][0] == 'O' && matrix[i][1] == 'O' && matrix[i][2] == 'O') {
                 return true; //checking rows
             }
         }
         for (int i = 0; i < 3; i++) {
-            if (matrix[0][i] == 'O' && matrix [1][i] == 'O' && matrix [2][i] == 'O') {
+            if (matrix[0][i] == 'O' && matrix[1][i] == 'O' && matrix[2][i] == 'O') {
                 return true; //checking columns
             }
         }
-        if (matrix[0][0] == 'O' && matrix [1][1] == 'O' && matrix [2][2] == 'O') {
+        if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O') {
             return true;
         }
-        if (matrix[0][2] == 'O' && matrix [1][1] == 'O' && matrix [2][0] == 'O') {
+        if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O') {
             return true; //checking diagonals
         }
         return false; //no winnings for o
@@ -62,8 +62,23 @@ public class Board {
     }
 
     public String getWinner() {
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0] != null && matrix[i][0].equals(matrix[i][1]) && matrix[i][0].equals(matrix[i][2])) {
+                return matrix[i][0].toString(); //checking rows for winner
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[0][i] != null && matrix[0][i].equals(matrix[1][i]) && matrix[0][i].equals(matrix[2][i])) {
+                return matrix[0][i].toString(); //checking columns for winner
+            }
 
-        return null;
+        }
+        if (matrix[0][0] != null && matrix[0][0].equals(matrix[1][1]) && matrix[0][0].equals(matrix[2][2])) {
+            return matrix[0][0].toString();
+        }
+        if (matrix[0][2] != null && matrix[0][2].equals(matrix[1][1]) && matrix[0][2].equals(matrix[2][0])) {
+            return matrix[0][2].toString(); //checking diagonals for winner
+        }
+        return "";
     }
-
 }
