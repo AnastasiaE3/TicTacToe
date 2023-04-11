b7ba4f471c82d53928f813c06eca63ca4ab73112
AnastasiaE3 <125825396+AnastasiaE3@users.noreply.github.com>
2023-04-11 18:52:46 -0400

    isTie passed

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -51,8 +51,14 @@ public class Board {
     }
 
     public Boolean isTie() {
-
-        return null;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == null) {
+                    return false;
+                }
+            }
+        }
+        return !isInFavorOfX() && !isInFavorOfO();
     }
 
     public String getWinner() {
